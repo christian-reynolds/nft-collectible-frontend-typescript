232bcf2697cc720edad764d938430bce2069381f
christian-reynolds <93774940+christian-reynolds@users.noreply.github.com>
2021-12-17 11:01:45 -0500

    Breaking up components and adding web3-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,110 +1,23 @@
-import { ethers } from 'ethers';
+import { ethers, providers } from 'ethers';
 import { useEffect, useState } from 'react';
 import './App.css';
-import contract from './contracts/VolcanoToken.json';
+import { Web3ReactProvider } from '@web3-react/core'
+import Wallet from './components/Wallet';
 
-const contractAddress = "0x008D157915CAF7e3e8259A6e3A7507dfa35F6908";
-const abi = contract.abi;
+const getLibrary = (provider: any) => new providers.Web3Provider(provider);
 
 declare let window: {
   ethereum: any
 };
 
 function App() {
-  const [currentAccount, setCurrentAccount] = useState(null);
-
-  const checkWalletIsConnected = async () => { 
-    const { ethereum } = window;
-
-    if (!ethereum) {
-      console.log("Make sure you have Metamask installed!");
-      return;
-    } else {
-      console.log("Wallet exists!");
-    }
-
-    const accounts = await ethereum.request({ method: 'eth_accounts' });
-
-    if (accounts.length !== 0) {
-      const account = accounts[0];
-      console.log("Found an authorized account: ", account);
-      setCurrentAccount(account);
-    } else {
-      console.log("No authorized account found");
-    }
-  }
-
-  const connectWalletHandler = async () => { 
-    const { ethereum } = window;
-
-    if (!ethereum) { 
-      alert("Please install Metamask!");
-    }
-
-    try {
-      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-      console.log("Found an account! Address: ", accounts[0]);
-      setCurrentAccount(accounts[0]);
-
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  const mintNftHandler = async () => { 
-    try {
-      const { ethereum } = window;
-
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const nftContract = new ethers.Contract(contractAddress, abi, signer);
-
-        console.log("Initialize payment");
-        let nftTx = await nftContract.mint();
-        
-        console.log("Minting...please wait");
-        await nftTx.wait();
-
-        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTx.hash}`);
-
-
-      } else {
-        console.log("Ethereum object does not exist");
-      }
-
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  const connectWalletButton = () => {
-    return (
-      <button onClick={connectWalletHandler} className='cta-button connect-wallet-button'>
-        Connect Wallet
-      </button>
-    )
-  }
-
-  const mintNftButton = () => {
-    return (
-      <button onClick={mintNftHandler} className='cta-button mint-nft-button'>
-        Mint NFT
-      </button>
-    )
-  }
-
-  useEffect(() => {
-    checkWalletIsConnected();
-  }, [])
-
   return (
-    <div className='main-app'>
-      <h1>World's Best NFT</h1>
-      <div>
-        {currentAccount ? mintNftButton() : connectWalletButton()}
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <div className='main-app'>
+        <h1>World's Best NFT</h1>
+        <Wallet />
       </div>
-    </div>
+    </Web3ReactProvider>
   );
 }
 
---
+++ src/components/Wallet.tsx
@@ -0,0 +1,109 @@
+import { ethers, providers } from 'ethers';
+import { useWeb3React } from '@web3-react/core';
+import { useEffect, useState } from 'react';
+import contract from '../contracts/VolcanoToken.json';
+
+const contractAddress = "0x008D157915CAF7e3e8259A6e3A7507dfa35F6908";
+const abi = contract.abi;
+
+declare let window: {
+    ethereum: any
+};
+
+function Wallet() {
+    const [currentAccount, setCurrentAccount] = useState(null);
+    const { account, library, activate } = useWeb3React<providers.Web3Provider>();
+
+    const checkWalletIsConnected = async () => { 
+      const { ethereum } = window;
+  
+      if (!ethereum) {
+        console.log("Make sure you have Metamask installed!");
+        return;
+      } else {
+        console.log("Wallet exists!");
+      }
+  
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+  
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account: ", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
+    }
+  
+    const connectWalletHandler = async () => { 
+      const { ethereum } = window;
+  
+      if (!ethereum) { 
+        alert("Please install Metamask!");
+      }
+  
+      try {
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        console.log("Found an account! Address: ", accounts[0]);
+        setCurrentAccount(accounts[0]);
+  
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  
+    const mintNftHandler = async () => { 
+      try {
+        const { ethereum } = window;
+  
+        if (ethereum) {
+          const provider = new ethers.providers.Web3Provider(ethereum);
+          const signer = provider.getSigner();
+          const nftContract = new ethers.Contract(contractAddress, abi, signer);
+  
+          console.log("Initialize payment");
+          let nftTx = await nftContract.mint();
+          
+          console.log("Minting...please wait");
+          await nftTx.wait();
+  
+          console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTx.hash}`);
+  
+  
+        } else {
+          console.log("Ethereum object does not exist");
+        }
+  
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  
+    const connectWalletButton = () => {
+      return (
+        <button onClick={connectWalletHandler} className='cta-button connect-wallet-button'>
+          Connect Wallet
+        </button>
+      )
+    }
+  
+    const mintNftButton = () => {
+      return (
+        <button onClick={mintNftHandler} className='cta-button mint-nft-button'>
+          Mint NFT
+        </button>
+      )
+    }
+  
+    useEffect(() => {
+      checkWalletIsConnected();
+    }, [])
+
+    return (
+        <div>
+            {currentAccount ? mintNftButton() : connectWalletButton()}
+        </div>
+    );
+}
+
+export default Wallet;
